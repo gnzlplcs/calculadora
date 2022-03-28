197eecdd6e1114fec6bf73d2ac1ea3612d07f2f9
gnzlplcs <palaciosgm@outlook.com>
2022-03-28 17:39:47 -0500

    ➕ add logic to Boton.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Boton from "./componentes/Boton";
 import "./App.css";
 import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
 
@@ -12,7 +13,10 @@ function App() {
         />
       </div>
       <div className="contenedor-calculadora">
-        <div className="fila"></div>
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
         <div className="fila"></div>
         <div className="fila"></div>
         <div className="fila"></div>
