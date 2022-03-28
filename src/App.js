178c7f3ad5640e14971abc89746b5cbdfa8e4b6f
gnzlplcs <palaciosgm@outlook.com>
2022-03-27 22:26:05 -0500

    :lipstick: add contenedor-calculadora style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          alt="Logo de freeCodeCamp"
+          className="freecodecamp-logo"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+        <div className="fila"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
